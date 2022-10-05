75d04e81ab158cd81d99b0cae3c86684013d495e
izazi <nurulitqanizazi@gmail.com>
2022-10-05 14:11:49 +0800

    second commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,56 @@
+    <footer class="py-5 border-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 text-center text-sm-start mb-3">
+            <a href="{{ route('home') }}">
+              
+            </a>
+            <p class="title-alt mt-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, aut.
+            </p>
+          </div>
+          <div class="col-lg-3 mb-3">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="#" class="title">Home</a>
+              </li>
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="#" class="title">Payment</a>
+              </li>
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="#" class="title">Order</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 mb-3">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="#" class="title">Facebook</a>
+              </li>
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="#" class="title">Instagram</a>
+              </li>
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="#" class="title">Youtube</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-3 mb-3">
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item" style="border-bottom: none">
+                <a href="mailto:polong4212@gmail.com" class="title"
+                  >example@gmail.com</a
+                >
+              </li>
+              <li class="list-group-item" style="border-bottom: none">
+                <span class="title"></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="container-fluid text-center py-4 border-top mt-4">
+          <small>@Copyright • All reserved</small>
+        </div>
+      </div>
+    </footer>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,12 +15,30 @@
 */
 
 //Route::get('/', function () {
-//   return view('homepage');
+  //  return view('welcome');
 //});
-Route::resource('/', 'HomeController');
-Route::resource('about', 'AboutController');
-Route::resource('travel', 'TravelController');
-Route::resource('form', 'EmailController');
 
-Route::get('/email', [App\Http\Controllers\EmailController::class, 'create']);
-Route::post('/email', [App\Http\Controllers\EmailController::class, 'sendEmail'])->name('send.email');
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\PageController::class, 'home'])->name('home');
+Route::get('posts', [App\Http\Controllers\PageController::class, 'posts'])->name('posts');
+Route::get('posts/{post:slug}', [App\Http\Controllers\PageController::class, 'detailPost'])->name('posts.show');
+Route::get('paket-travel', [App\Http\Controllers\PageController::class, 'package'])->name('package');
+Route::get('detail/{travelPackage:slug}', [App\Http\Controllers\PageController::class, 'detail'])->name('detail');
+
+
+Route::get('kontak-kami', [App\Http\Controllers\PageController::class, 'contact'])->name('contact');
+Route::post('kontak-kami', [App\Http\Controllers\PageController::class, 'getEmail'])->name('contact.email');
+
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::group(['middleware' => 'isAdmin', 'prefix' => 'admin', 'as' => 'admin.'], function() {
+        Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('posts', \App\Http\Controllers\Admin\PostController::class);
+        Route::resource('cars', \App\Http\Controllers\Admin\CarController::class);
+        Route::resource('categories', \App\Http\Controllers\Admin\CategoryController::class);
+        Route::resource('travel-packages', \App\Http\Controllers\Admin\TravelPackageController::class);
+        Route::resource('travel-packages.galleries', \App\Http\Controllers\Admin\GalleryController::class);
+    });
+    
+});
